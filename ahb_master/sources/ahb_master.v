bf68898fc0e2df744d17117ffa70f7ac060940d9
krevanth <revanth91kamaraj@gmail.com>
2017-02-15 21:58:47 +0530

    added lic

--- ahb_master/sources/ahb_master.v
+++ ahb_master/sources/ahb_master.v
@@ -12,8 +12,30 @@
  * The entire design is driven by a single clock i.e., AHB clock. A global
  * asynchronous active low reset is provided.
  *
- * NOTE: THE DESIGN IS IN AN EXPERIMENTAL STATE.
- *****************************************************************************/
+ * ------->       NOTE: THE DESIGN IS IN AN EXPERIMENTAL STATE.   <----------
+ *****************************************************************************
+ * MIT License
+ * 
+ * Copyright (C) 2017 Revanth Kamaraj
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************/
 
 module ahb_master #(parameter DATA_WDT = 32, parameter BEAT_WDT = 32) (
 
